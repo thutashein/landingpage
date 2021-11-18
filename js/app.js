7627dfe28d12ab292532131cdbb0bc6ca78767f3
thutanyan <thutathuta319@gmail.com>
2021-11-18 13:55:53 +0630

    11/17/2021

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    // console.log('hay');
+
+    // START SHOWCASE SLIDER SECTION
+    $('.carousel').carousel({
+        interval: 6000,
+        pause: 'hover',
+    });
+});
+
+
+
+
+
+
